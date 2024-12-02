501ea9f36b80466042c720b3c3df14cf1a54c7b2
Kartik-Banga <kartikbanga@gmail.com>
2024-12-02 18:39:21 -0500

    Adding Procfile Modified

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
+import os
 
 load_dotenv()
 
@@ -21,7 +22,10 @@ app.include_router(patientSugarLevel.router, prefix="/patientSugarLevel", tags=[
 app.include_router(factorsOfHeartDiseases.router, prefix = "/factorsOfHeartDiseases", tags=["factorsOfHeartDiseases"])
 app.include_router(lungCancer.router, prefix = "/lungCancer", tags=["lungCancer"])
 
-
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8000))  # Heroku sets PORT
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 
 
